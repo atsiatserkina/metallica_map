44855fb2f4dc5509c414e1b1044ae52aae879ceb
Aliaksei_Maiseyenkau <amaiseyenkau@newscorp.com>
2022-11-23 20:56:56 +0400

    MapboxStyleId

--- lib/AppConstants.dart
+++ lib/AppConstants.dart
@@ -1,7 +1,7 @@
 import 'package:latlong2/latlong.dart';
 
 class AppConstants {
-  static const String mapBoxAccessToken = 'YOUR_ACCESS_TOKEN';
-  static const String mapBoxStyleId = 'YOUR_STYLE_ID';
+  static const String mapBoxAccessToken = 'pk.eyJ1IjoiYWxleG1vIiwiYSI6ImNqdGp6b2N0YjFtbXQ0M3JyNW9rdHkxb2sifQ.Yq1qaGR4g05O0Pug4hMvnA';
+  static const String mapBoxStyleId = 'clatu0m5b003i15mwitesahsc';
   static final startLocation = LatLng(51.5090214, -0.1982948);
 }
