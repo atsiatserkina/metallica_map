f29870fce923a4d125938b05077a2b81cb6cb803
Aliaksei_Maiseyenkau <amaiseyenkau@newscorp.com>
2022-11-24 19:36:54 +0400

    Update geojson path

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 class MapConstants {
   static const String mapBoxToken = 'pk.eyJ1IjoiYWxleG1vIiwiYSI6ImNqdGp6b2N0YjFtbXQ0M3JyNW9rdHkxb2sifQ.Yq1qaGR4g05O0Pug4hMvnA';
   static const String jsonData = 'assets/metallica_concerts.json';
-  static const String geoJsonData = 'assets/metallica_concerts.geojson';
+  static const String geoJsonData = 'assets/assets/metallica_concerts.geojson';
   static const String concertSourceId = 'metallica-concerts';
   static const String concertClusterLayerID = 'metallica-concerts-circles';
   static const String concertCountLayerID = 'metallica-concerts-count';
